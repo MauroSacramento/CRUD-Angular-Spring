b50182cad27504bf4b290076282de956f7a72aad
Maurosacramento <maurosacramento35@gmail.com>
2025-01-24 20:04:04 +0000

    display data from course.jon

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,26 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import {MatTableModule} from '@angular/material/table';
+import {MatCardModule} from '@angular/material/card';
+import {MatToolbarModule} from '@angular/material/toolbar';
 import { Course } from '../model/course';
+import { CoursesService } from '../services/courses.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-courses',
-  imports: [MatTableModule],
+  imports: [MatTableModule, MatCardModule, MatToolbarModule],
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.scss'
 })
 export class CoursesComponent implements OnInit {
 
-  courses: Course[] = [
-    {
-      _id: '1',
-      name: 'Angular',
-      categoria: 'Front-end'
-    }
-  ];
+  courses: Course[] = [];
   displayedColumns = ['name', 'category']
+
+  constructor(private courseService: CoursesService){
+    this.courseService.getList().subscribe(course => this.courses = course);
+  }
 
   ngOnInit(): void {
 
   }
+
 
 }
