cf70b292fedff817ea59afc65256949890b173b2
Maurosacramento <maurosacramento35@gmail.com>
2025-02-02 20:27:20 +0000

    Up to date

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,16 @@
 import { Routes } from '@angular/router';
 import { CoursesComponent } from './courses/courses/courses.component';
 
+
 export const routes: Routes = [
   {path: '', pathMatch: 'full', redirectTo: 'courses'},
-  {path: 'courses', loadComponent: () => import('./courses/courses/courses.component').then((mod) => mod.CoursesComponent)}
+  {
+    path: 'courses',
+    //loadComponent: () => import('./courses/courses/courses.component').then((mod) => mod.CoursesComponent),
+    component: CoursesComponent,
+    children: [
+      { path: 'new', loadComponent: () => import('./courses/course-form/course-form.component').then((mod) => mod.default)}
+    ]
+  },
   //{path: 'courses', component: CoursesComponent}
 ];
--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,30 +1,42 @@
+import { routes } from './../../app.routes';
 import { CoursesService } from './../services/courses.service';
 import { Component, DestroyRef, inject, OnInit, Signal, signal, PipeTransform } from '@angular/core';
 import {MatTableModule} from '@angular/material/table';
 import {MatCardModule} from '@angular/material/card';
 import {MatIconModule} from '@angular/material/icon';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
 import { Course } from '../model/course';
 import { Observable, catchError } from 'rxjs';
 import { CategoryPipe } from '../../shared/pipes/category.pipe';
+import { ActivatedRoute, Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-courses',
-  imports: [MatTableModule, MatCardModule, MatToolbarModule, MatIconModule, CategoryPipe],
+  imports: [MatTableModule, MatCardModule, MatToolbarModule, MatIconModule, CategoryPipe, RouterOutlet],
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.scss'
 })
 export class CoursesComponent {
 
+  //private router = inject(Router);
   private destroyRef = inject(DestroyRef);
   private CoursesService = inject(CoursesService);
 
   courses : Course[] = [];
-  displayedColumns = ['name', 'category']
+  displayedColumns = ['name', 'category', 'action'];
 
-  constructor(){
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router
+  ){
+
     this.CoursesService.getList().subscribe(courses => this.courses = courses);
     console.log(this.courses)
+  }
+
+  onAdd(){
+    this.router.navigate(['new'], {relativeTo: this.route});
   }
 
 }
